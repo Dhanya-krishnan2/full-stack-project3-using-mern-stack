840c85b6b994f6baaee505e348c02cd57dc39d63
Dhanya-krishnan2 <dhanyakunhi@gmail.com>
2020-10-05 01:58:23 +0800

    added the controllers for event and orders

---
+++ README.md
@@ -0,0 +1,2 @@
+## ORDER ME
+** This application is a  mern-fullstack, because it is made up of mongodb, express, react and node js, hence the name MERN, here I made a client folder by using react
---
+++ controllers/eventsController.js
@@ -0,0 +1,52 @@
+const db = require("../models");
+
+// Defining methods for the eventsController
+module.exports = {
+  findAll: function(req, res) {
+    db.Event
+      .find(req.query)
+      .sort({ eventDateTime: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  loadEventsByHost: function(req, res) {
+    console.log("host id: ", req.params.id)
+    db.Event
+      .find({hostId: req.params.id})
+      .sort({ eventDateTime: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findAllOrders: function(req, res) {
+    db.Order
+      .find({eventID: req.params.id})
+      .sort({ OrderDateTime: -1 })
+      .then(dbModels => res.json(dbModels))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    db.Event
+      .findOne({_id: req.params.id})
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    db.Event
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  update: function(req, res) {
+    db.Event
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    db.Event
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+};
