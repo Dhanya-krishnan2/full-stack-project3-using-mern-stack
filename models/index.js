f1f5f59ba049c20eda387f2174bd8121ccfe404c
Dhanya-krishnan2 <dhanyakunhi@gmail.com>
2020-10-05 21:43:45 +0800

    added all the  shemas

---
+++ models/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+    Event: require("./event"),
+    Order: require("./order"),
+    User: require('./User'),
+    UserSession: require('./UserSession'),
+  };
---
+++ models/order.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const OrderSchema = new Schema({
+  eventID:{ type: String, required: true },
+  email:{ type: String, required: true  },
+  name:{ type: String, default: "" }, 
+  foodOrder: { type: String, default: "" },
+  specialRequest: { type: String, default: "" },
+  price:{ type: Number, default: 0 },
+  date: { type: String, default: "" }
+});
+
+const Order = mongoose.model("Order", OrderSchema);
+
+module.exports = Order;
