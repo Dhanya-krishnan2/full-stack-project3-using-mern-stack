6722017b6cc7d074bd6e9988ef7b9a6c9940bbeb
Dhanya-krishnan2 <dhanyakunhi@gmail.com>
2020-10-06 18:48:13 +0800

    added server.js and downloaded all the dependancies

---
+++ config/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    db: 'mongodb://username:password@url:port/db',
+    db_dev: 'mongodb://localhost:27017/order_me_this_db',
+  };
+  
---
+++ server.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const cookieSession = require("cookie-session");
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3035;
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieSession({
+  httpOnly: true,
+  keys: ['token'],
+  name: 'session',
+}))
+
+// Add routes, both API and view
+app.use(routes);
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, "client/build/index.html"));
+  })
+  console.log("test");
+}
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/order_me_this_db");
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
